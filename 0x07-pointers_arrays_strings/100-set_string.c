a6c183451a2b4ed4a07e46476fa8d457089152fd
AymanChatki1 <chatkiayman@gmail.com>
2023-10-23 09:51:34 -0700

    more pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function for two pointers
+ * @s: pointer to pointer char
+ * @to: pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
